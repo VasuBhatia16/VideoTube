cb6b4b8f0a89a51422ababbf6d81a3bde9a883a7
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-12-05 14:48:57 +0530

    connect to mongoose atlas

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// require('dotenv').config({path: './env'})
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app =  express()
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error: ",error);
+            throw error
+            
+        })
+
+        app.listen(process.env.PORT,()={
+            console.log(`App is listening on ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("ERROR: ",error);
+        throw error
+    }
+} )()
+
+*/
