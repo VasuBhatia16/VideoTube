97182d7e8d62a2c72c013cff6e5c2c6abd720281
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-12-11 20:36:40 +0530

    Bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
-import {User} from '../models/user.models.js'
+import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {ApiResponse} from '../utils/ApiResponse.js'
 
@@ -26,19 +26,23 @@ const registerUser = asyncHandler( async(req,res)=>{
     if([fullName,email,password,username].some((field)=> field?.trim()==="")){
         throw new ApiError(400, "All fields are compulsory")
     }
-    const existedUser = User.findOne({$or:[{username},{email}]})
+    const existedUser = await User.findOne({$or:[{username},{email}]})
     if (existedUser){
         throw new ApiError(409, "Username/Email already exists")
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
-
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        const coverImageLocalPath = req.files?.coverImage[0]?.path
+    }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required")
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
 
     if(!avatar){
         throw new ApiError(400,"Avatar is required")
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,10 +44,6 @@ const userSchema = new Schema(
         },
         refreshToken:{
             type:String,
-            required:true,
-            unique:true,
-            lowercase: true,
-            trim:true,
         },
     }
 ,{timestamps:true})
@@ -59,22 +55,28 @@ userSchema.pre("save",async function (next) {
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    await bcrypt.compare(password,this.password);
+    return await bcrypt.compare(password,this.password);
 }
 
-userSchema.methods.generateAccessToken() = function(){
-    jwt.sign({
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
         _id:this._id,
         email:this.email,
         username:this.username,
-        fullname:this.fullname
-    },process.env.ACCESS_TOKEN_SECRET,{
+        fullname:this.fullName
+    }
+    ,
+    process.env.ACCESS_TOKEN_SECRET
+    ,
+    {
         expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-    })
+    }
+)
 }
 
-userSchema.methods.generateRefreshToken() = function(){
-    jwt.sign({
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
         _id:this._id,
     },process.env.REFRESH_TOKEN_SECRET,{
         expiresIn:process.env.REFRESH_TOKEN_EXPIRY
